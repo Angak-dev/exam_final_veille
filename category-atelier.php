25086c8519efa4dbba849d42b8add03062ae9d02
Bourassa <bourassa30@gmail.com>
2020-03-09 11:31:02 -0400

    Commit 4 non-complet

--- category-atelier.php
+++ category-atelier.php
@@ -1,4 +1,4 @@
-<?php /*Template name: category_atelier*/ 
+<?php 
     
     $args = array(
         'category_name' => 'Atelier',
@@ -12,16 +12,51 @@
 ?>
 
 <?php
-    echo '<div class="categorie-atelier">';
     echo '<h1>Nos atelier de perfectionnements sont offerts 1 fois par mois</h1>';
+    echo '<div class="grille-atelier">';
+    
+    if(have_posts()):
+        echo '<div class="grille-atelier">';
+        while (have_posts() ) : the_post(); 
 
-    while ($queryAtelier->have_posts()) {
-        $queryAtelier->the_post();
-        echo '
-            <div class="content-post">
-                <p class="titre_atelier">' .get_the_title(). '__<span style=color:red;>' .get_post_field('post_name'). '</span>__<span style=color:blue;>' .get_the_author_meta('display_name', $post->post_author ).'</span></p> 
-            </div>
-        ';
+            $postTitle = get_the_title();
+            echo 'postTitle';
+
+            $postname = get_post_field('post_name');
+            echo 'postname';
+
+            $postAuthor = get_the_author_meta('display_name', $post->post_author);  
+            echo 'postAuthor';
+
+            $gridArea = '.$postTitle.'/'.$postname.'/'.$postAuthor.';
+
+            echo '
+                <div class="content-post">
+                    <p>Grid : '.$gridArea. " " .substr(get_post_field('post_name'),2,4).'</p>
+                </div>
+            ';
+        endwhile;
+        echo '</div>';
+    endif;
+
+    function positionGrid($postAuthor){
+        switch ($postAuthor) {
+            case Luna:
+                return 1;
+                break;
+            case Eddy:
+                return 2;
+                break;
+            case Derick:
+                return 3;
+                break;
+            case Maybell:
+                return 4;
+                break;
+            default: 
+                return 0;
+                break;
+        }
     }
 ?>
 
